cebac7fd0f13054f870d01f9ea8a3250071d5274
Qubacki01 <erazmusk@windowslive.com>
2024-11-17 14:52:34 +0100

    x.x

--- kuba Erazmus/8-4.py
+++ kuba Erazmus/8-4.py
@@ -2,7 +2,7 @@
 # A program that prints your height both in cm and in feet and inches.
 #
 cm = int(input("Enter your height (in cm): "))
-feet = cm//30.48
+feet = f'{cm//30.48:.0f}'           # <-- The f'{...:.0f}' rounds the number to 0 decimal places
 inches = f'{(cm%30.48)/2.54:.0f}'   # <-- The f'{...:.0f}' rounds the number to 0 decimal places
 
 print(f'I am {cm}cm tall, i.e. {feet}ft  and {inches} inches.')
